03bae846ef3162283279c8b6697561de0ffdabd2
Kageyama Tobio <kageyamatobio@Kageyamas-MacBook-Air.local>
2021-09-29 12:31:36 -0400

    closes out previous window automatically

--- background.js
+++ background.js
@@ -1,29 +1,47 @@
+
 function getSelectedText() {
   let selectedText = window.getSelection().toString()
   chrome.storage.sync.set({selectedText})
-    
-  
 }
-chrome.commands.onCommand.addListener(function(command, tab) {
+chrome.commands.onCommand.addListener(async function(command, tab) {
   console.log("Command:", command);
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
     func: getSelectedText,
   });
-  chrome.tabs.create({
-    url: chrome.runtime.getURL('popout.html'),
-    active: false,
-}, function(tab) {
-    // After the tab has been created, open a window to inject the tab
-    chrome.windows.create({
-        tabId: tab.id,
-        type: 'popup',
-        focused: true,
-        height: 500,
-        width: 500,
-        left: 0,
-        // incognito, top, left, ...
-    });
-});
-});
-
+  let url = 'popout.html'
+  let tabId
+  chrome.windows.getAll({ populate: true }).then((windows) => {
+   
+      for (let i = 0; i < windows.length; i++){
+      console.log(windows[i].tabs)
+      for(let j = 0; j < windows[i].tabs.length; j++){
+        console.log(windows[i].tabs[j].url)
+        if(windows[i].tabs[j].url.includes(url)) {
+          console.log(windows[i].tabs[j].id)
+          return windows[i].tabs[j].id
+        }
+      }
+    }
+    return 0
+  }).then((tabId) => {
+    console.log(tabId)
+    if (tabId) {
+      chrome.tabs.remove(tabId, function(){});
+    }
+      chrome.tabs.create({
+        url: chrome.runtime.getURL(url),
+        active: false,
+    }, function(tab) {
+        chrome.windows.create({
+            tabId: tab.id,
+            type: 'popup',
+            focused: true,
+            height: 500,
+            width: 500,
+            left: 0,
+            // incognito, top, left, ...
+        });
+      })
+  })
+  })
--- options.js
+++ options.js
@@ -4,6 +4,10 @@ let textSpeedInput = document.getElementById('textspeed')
 let fontSizeInput = document.getElementById('fontsize')
 let setTextSpeed = document.getElementById('setTextSpeed')
 let setFontSize = document.getElementById('setFontSize')
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
 let fontSize = 30
 let speed = 300
 let temp
